0eb0b1d736adb576da500119c92d7b998eff9ae5
qqaazz800624 <qqaazz800624@gmail.com>
2024-09-04 18:38:22 +0800

    update cout and maintain fixed floating precision

--- kinetic_energy.cpp
+++ kinetic_energy.cpp
@@ -4,6 +4,7 @@ It shows a message on the console.
 */
 # include <cstdio>
 # include <iostream> // header for std::cout
+# include <iomanip> // header for std::setprecision
 using namespace std;
 
 double kinetic_energy(int, double);
@@ -12,7 +13,8 @@ int main() // program entry
 {
     double energy;
     energy = kinetic_energy(15, 300);
-    //printf("The value of the kinetic energy is: %.3f", energy);
+    printf("The value of the kinetic energy is: %.3f\n", energy);
+    std::cout << std::fixed << std::setprecision(3);
     std::cout << "The value of the kinetic energy is: " << energy << std::endl;
     cout << "The value of the kinetic energy is: " << energy << endl;
     return 0;

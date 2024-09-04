37223a03c9da945204c30896adcad419b72ff378
qqaazz800624 <qqaazz800624@gmail.com>
2024-09-04 17:55:19 +0800

    add the first test script written in C++

---
+++ kinetic_energy.cpp
@@ -0,0 +1,26 @@
+/*
+This is my first C++ program!
+It shows a message on the console.
+*/
+# include <cstdio>
+# include <iostream> // header for std::cout
+using namespace std;
+
+double kinetic_energy(int, double);
+// The main function
+int main() // program entry
+{
+    double energy;
+    energy = kinetic_energy(15, 300);
+    printf("The value of the kinetic energy is: %.3f", energy);
+    std::cout << "The value of the kinetic energy is: " << energy << std::endl;
+    cout << "The value of the kinetic energy is: " << energy << endl;
+    return 0;
+}
+// user-defined function
+double kinetic_energy(int m, double v)
+{
+    double ke;
+    ke = 0.5 * m * v * v;
+    return ke;
+}

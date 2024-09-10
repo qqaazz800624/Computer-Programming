94bad8e25810760a54672e14cc0b53a2c88ec4c1
qqaazz800624 <qqaazz800624@gmail.com>
2024-09-10 11:15:03 +0800

    switch case statement

--- ch2/switch_case.c
+++ ch2/switch_case.c
@@ -1,8 +1,33 @@
 #include <stdio.h>
 
+
 int main()
 {
-    
+    double n1, n2, ans;
+    char op;
+    scanf("%lf", &n1);
+    scanf("%lf", &n2);
+    scanf(" %c", &op);  // space before %c is necessary to skip the newline character
+
+    switch (op)
+    {
+    case '+':
+        ans = n1 + n2;
+        break;
+    case '-':
+        ans = n1 - n2;
+        break;
+    case '*':
+        ans = n1 * n2;
+        break;
+    case '/':
+        ans = n1 / n2;
+        break;
+    default:
+        printf("Invalid operator\n");
+        break;
+    }
+    printf("%.2f %c %.2f = %.2f\n", n1, op, n2, ans);
 
     return 0;
 }

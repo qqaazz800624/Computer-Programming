66aca71e386b342a2c862154d88658becd191263
qqaazz800624 <qqaazz800624@gmail.com>
2024-09-16 15:25:46 +0800

    update No. 1012

--- ch2/hw1_1012.c
+++ ch2/hw1_1012.c
@@ -3,10 +3,22 @@
 int main()
 {
     int score, grade;
-    printf("(1) 大學部 (2) 研究所：");
+    //printf("(1) 大學部 (2) 研究所：");
     scanf("%d", &grade);
-    printf("請輸入您的分數：");
+    if (grade != 1 && grade != 2)
+    {
+        printf("role error\n");
+        return 0;
+    }
+
+    //printf("請輸入您的分數：");
     scanf("%d", &score);
+
+    if (score < 0 || score > 100)
+    {
+        printf("score error\n");
+        return 0;
+    }
 
     if (score < 60 && grade ==1){  // grade == 1 is actually not necessary
         printf("fail\n");

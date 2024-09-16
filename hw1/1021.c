6f772e5ce64f925ffe858c0cfc47c6c9bca9c5b2
qqaazz800624 <qqaazz800624@gmail.com>
2024-09-16 16:46:02 +0800

    update 1021

--- hw1/1021.c
+++ hw1/1021.c
@@ -11,13 +11,14 @@ int main()
     // Descending triangle with spaces
     for (row = 0; row < num_rows; row++){
         for (spaces = 0; spaces < row; spaces++){
-            printf("  "); // Two spaces to align with the width of a "* "
+            printf(" "); //  spaces to align with the width of a "*"
         }
         for (col = 0; col < num_rows - row; col++){
-            printf("* ");
+            printf("*");
         }
         printf("\n");
     }
     
     return 0;
 }
+

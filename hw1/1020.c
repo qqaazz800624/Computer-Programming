015e66be345a0fde2ecae825d0c8af418992bd2b
qqaazz800624 <qqaazz800624@gmail.com>
2024-09-17 15:29:39 +0800

    add No. 1020

---
+++ hw1/1020.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+int main(){
+    
+    int n, left, extra, count;
+    int chance = 1;
+    scanf("%d", &n);
+
+    left = n%4;
+    extra = n/4;
+    count = n;
+    count += extra;
+
+    while ((left + extra) >=4){
+        n = left + extra;
+        left = n%4;
+        extra = n/4;
+        count += extra;
+    }
+    if (left + extra == 3){
+        count += 1;
+    }
+    printf("%d\n", count);
+
+    return 0;
+}

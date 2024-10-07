b93705ea4e30a8bb3294a140f74fc614d037dc70
qqaazz800624 <qqaazz800624@gmail.com>
2024-10-07 11:53:46 +0800

    add feof for char

---
+++ ch8/feof_char.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+
+    FILE *f1;
+    char str[128];
+    scanf("%s", str);
+    f1 = fopen(str, "r");
+    
+    char ch;
+
+    while (1){
+        fscanf(f1, "%c", &ch); // Don't put a space after %c
+        if (feof(f1)){
+            break;
+        }
+        putchar(ch);
+
+    }
+
+    fclose(f1);
+
+    return 0;
+};

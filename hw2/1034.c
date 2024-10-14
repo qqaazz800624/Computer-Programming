269eefec88561e7f432af14720f49befd7475bc7
qqaazz800624 <qqaazz800624@gmail.com>
2024-10-14 22:29:42 +0800

    add No. 1034

---
+++ hw2/1034.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+#define N 5
+int main() 
+{
+ int array[N];
+ int i,j;
+ 
+ 
+ for(i=0;i<N;i++){
+  scanf("%d",&array[i]);
+ }
+
+ for(i=0;i<N;i++){
+  printf("%d\t",array[i]);
+  for(j=0;j<array[i];j++){
+   printf("*");
+  }
+  puts("");
+ }
+ 
+ return 0;
+}

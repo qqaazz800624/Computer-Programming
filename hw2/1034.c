e9c8230e74806fdb5a6542796f7f17155905f05d
qqaazz800624 <qqaazz800624@gmail.com>
2024-10-14 22:36:48 +0800

    update No. 1034

--- hw2/1034.c
+++ hw2/1034.c
@@ -2,25 +2,21 @@
 #include <stdlib.h>
 #include <string.h>
 
-
 #define N 5
-int main() 
-{
- int array[N];
- int i,j;
- 
- 
- for(i=0;i<N;i++){
-  scanf("%d",&array[i]);
- }
 
- for(i=0;i<N;i++){
-  printf("%d\t",array[i]);
-  for(j=0;j<array[i];j++){
-   printf("*");
-  }
-  puts("");
- }
- 
- return 0;
+int main(){
+    int array[N];
+    int i,j;
+    for(i=0;i<N;i++){
+        scanf("%d",&array[i]);
+    }
+    for(i=0;i<N;i++){
+        printf("%d\t",array[i]);
+        for(j=0;j<array[i];j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
 }
+

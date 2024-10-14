80bacb4dc63d32d1cc6e7ae9f158032afa7ae099
qqaazz800624 <qqaazz800624@gmail.com>
2024-10-14 22:46:19 +0800

    update No. 1135

--- hw2/1135.c
+++ hw2/1135.c
@@ -2,27 +2,25 @@
 #include <stdlib.h>
 
 
-int main() 
-{
- int N;
- int *array;
- int i;
- int ans;
- 
- scanf("%d",&N);
- 
- array=(int*)malloc(sizeof(int)*N);
- for(i=0;i<N;i++){
-  scanf("%d",array+i);
- }
- if (N%2==0){
-  ans=(array[N/2-1]+array[N/2]+1)/2;
- }
- else{
-  ans=array[N/2];
- }
- printf("%d",ans);
- puts("");
- free(array);
- return 0;
+int main(){
+    int n;
+    int *arr;
+    int i;
+    int ans;
+    scanf("%d", &n);
+    arr = (int*)malloc(sizeof(int)*n);
+    
+    for (i=0; i<n; i++){
+        scanf("%d", &arr[i]);
+    }
+
+    if(n%2==0){
+        ans = (arr[n/2-1]+arr[n/2]+1)/2;
+    }
+    else{
+        ans = arr[n/2];
+    }
+    printf("%d\n", ans);
+    free(arr);
+    return 0;
 }

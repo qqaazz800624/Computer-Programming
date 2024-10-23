8706f7faa86e27422c6dad3c71533c806c2c4725
qqaazz800624 <qqaazz800624@gmail.com>
2024-10-23 15:44:52 +0800

    update No. 3294

--- hw2/3294.c
+++ hw2/3294.c
@@ -2,72 +2,41 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAC_ADDR_LENGTH 20 // 17 characters + null terminator
+#define MAC_ADDR_LENGTH 18  
 
 int main() {
-    int n, i;
+    int n;
     
-    // Read the number of connected users
-    if (scanf("%d", &n) != 1) {
-        fprintf(stderr, "Error reading the number of connected users.\n");
-        return 1;
-    }
+    scanf("%d", &n);
     
-    // Consume the newline character after 'n'
-    getchar();
-    
-    // Allocate memory for n MAC addresses
     char **mac_addresses = (char **)malloc(n * sizeof(char *));
     if (mac_addresses == NULL) {
         fprintf(stderr, "Memory allocation failed for mac_addresses.\n");
         return 1;
     }
-    
-    // Read n MAC addresses
-    for (i = 0; i < n; ++i) {
-        mac_addresses[i] = (char *)malloc(MAC_ADDR_LENGTH);
+
+    for (int i = 0; i < n; ++i) {
+        mac_addresses[i] = (char *)malloc(MAC_ADDR_LENGTH * sizeof(char));
         if (mac_addresses[i] == NULL) {
             fprintf(stderr, "Memory allocation failed for mac_addresses[%d].\n", i);
             return 1;
         }
-        if (fgets(mac_addresses[i], MAC_ADDR_LENGTH, stdin) == NULL) {
-            fprintf(stderr, "Error reading MAC address %d.\n", i);
-            return 1;
-        }
-        // Remove newline character if present
-        size_t len = strlen(mac_addresses[i]);
-        if (len > 0 && mac_addresses[i][len - 1] == '\n') {
-            mac_addresses[i][len - 1] = '\0';
-        }
-    }
-    
-    size_t buffer_size = 1000000; // Adjust the size if needed
-    char *indices_line = (char *)malloc(buffer_size * sizeof(char));
-    if (fgets(indices_line, buffer_size, stdin) == NULL) {
-        fprintf(stderr, "Error reading indices line.\n");
-        free(indices_line);
-        return 1;
+        scanf("%s", mac_addresses[i]);
     }
 
-    
-    // Parse indices from the line
-    char *token = strtok(indices_line, " \n");
-    while (token != NULL) {
-        int idx = atoi(token);
-        if (idx >= 0 && idx < n) {
-            printf("%s\n", mac_addresses[idx]);
+    int index;
+    while (scanf("%d", &index) != EOF) {
+        if (index >= 0 && index < n) {
+            printf("%s\n", mac_addresses[index]);  
         } else {
-            fprintf(stderr, "Invalid query index: %d\n", idx);
+            fprintf(stderr, "Invalid index: %d\n", index);
         }
-        token = strtok(NULL, " \n");
     }
-    
-    // Free allocated memory
-    for (i = 0; i < n; ++i) {
-        free(mac_addresses[i]);
+
+    for (int i = 0; i < n; ++i) {
+        free(mac_addresses[i]);  
     }
-    free(mac_addresses);
-    free(indices_line);
-    
+    free(mac_addresses);  
+
     return 0;
 }

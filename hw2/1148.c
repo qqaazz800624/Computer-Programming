fdaf485f5270dcf9d1778b404937b50ec9ffd5e2
qqaazz800624 <qqaazz800624@gmail.com>
2024-10-23 11:31:53 +0800

    update No. 1148

--- hw2/1148.c
+++ hw2/1148.c
@@ -2,105 +2,107 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct {
-    char name[10];
-    int sides[3];
-} Triangle;
+#define NAME_LEN 10
+#define MAX_TRIANGLES 10000  // Define the maximum number of triangles
 
+// Function to compare strings (used in qsort)
 int compareNames(const void *a, const void *b) {
-    return strcmp(((Triangle *)a)->name, ((Triangle *)b)->name);
+    return strcmp(*(const char **)a, *(const char **)b);
+}
+
+// Function to print a list of names with the specified title
+void printList(const char *title, char **list, int size) {
+    printf("%s: ", title);
+    if (size > 0) {
+        for (int i = 0; i < size; i++) {
+            if (i > 0) printf(",");
+            printf("%s", list[i]);
+        }
+        printf("\n");
+    } else {
+        printf("None\n");
+    }
 }
 
 int main() {
-    Triangle triangles[100];
-    int triangleCount = 0;
+    char input[50];
+    char name[NAME_LEN];
+    int a, b, c;
 
-    // Input reading
+    // We use fixed arrays for storing the names, assuming a maximum number of triangles
+    char notTriangle[MAX_TRIANGLES][NAME_LEN];
+    char acuteAngle[MAX_TRIANGLES][NAME_LEN];
+    char obtuseAngle[MAX_TRIANGLES][NAME_LEN];
+    char rightAngle[MAX_TRIANGLES][NAME_LEN];
+
+    // Pointers to the arrays for sorting
+    char *notTrianglePtrs[MAX_TRIANGLES];
+    char *acuteAnglePtrs[MAX_TRIANGLES];
+    char *obtuseAnglePtrs[MAX_TRIANGLES];
+    char *rightAnglePtrs[MAX_TRIANGLES];
+
+    int notTriangleSize = 0, acuteSize = 0, obtuseSize = 0, rightSize = 0;
+
+    // Input reading loop
     while (1) {
-        char input[50];
-        fgets(input, sizeof(input), stdin);
+        // Read input
+        if (!fgets(input, sizeof(input), stdin)) {
+            break;  // End of input (in case of unexpected EOF)
+        }
 
+        // Check if the input is -1, which means we stop input
         if (strcmp(input, "-1\n") == 0) {
+            break;  // End the input when we get "-1"
+        }
+
+        // Parsing input into name, a, b, c
+        if (sscanf(input, "%s %d %d %d", name, &a, &b, &c) != 4) {
+            fprintf(stderr, "Invalid input format. Skipping line.\n");
+            continue;  // Skip invalid inputs
+        }
+
+        // Safety check: Ensure we don't exceed MAX_TRIANGLES
+        if (notTriangleSize >= MAX_TRIANGLES || acuteSize >= MAX_TRIANGLES ||
+            obtuseSize >= MAX_TRIANGLES || rightSize >= MAX_TRIANGLES) {
+            fprintf(stderr, "Exceeded maximum number of triangles. Stopping input.\n");
             break;
         }
 
-        sscanf(input, "%s %d %d %d",
-               triangles[triangleCount].name,
-               &triangles[triangleCount].sides[0],
-               &triangles[triangleCount].sides[1],
-               &triangles[triangleCount].sides[2]);
-
-        triangleCount++;
-    }
-
-    // Sort triangles by name
-    qsort(triangles, triangleCount, sizeof(Triangle), compareNames);
-
-    char notTriangle[1000] = "";
-    char acuteAngle[1000] = "";
-    char obtuseAngle[1000] = "";
-    char rightAngle[1000] = "";
-
-    // Classify triangles
-    for (int i = 0; i < triangleCount; i++) {
-        int a = triangles[i].sides[0];
-        int b = triangles[i].sides[1];
-        int c = triangles[i].sides[2];
-
-        // Sort a, b, c to make sure a <= b <= c
-        if (a > b) { int temp = a; a = b; b = temp; }
+        // Sort a, b, c such that a <= b <= c
+        if (a > c) { int temp = a; a = c; c = temp; }
         if (b > c) { int temp = b; b = c; c = temp; }
-        if (a > b) { int temp = a; a = b; b = temp; }
 
+        // Classify the triangle
         if (a + b <= c) {
-            // Not a triangle
-            strcat(notTriangle, triangles[i].name);
-            strcat(notTriangle, ",");
-        } else {
-            int a2b2 = a * a + b * b;
-            int c2 = c * c;
-
-            if (a2b2 > c2) {
-                strcat(acuteAngle, triangles[i].name);
-                strcat(acuteAngle, ",");
-            } else if (a2b2 < c2) {
-                strcat(obtuseAngle, triangles[i].name);
-                strcat(obtuseAngle, ",");
-            } else {
-                strcat(rightAngle, triangles[i].name);
-                strcat(rightAngle, ",");
-            }
+            strcpy(notTriangle[notTriangleSize], name);
+            notTrianglePtrs[notTriangleSize] = notTriangle[notTriangleSize];
+            notTriangleSize++;
+        } else if (a * a + b * b > c * c) {
+            strcpy(acuteAngle[acuteSize], name);
+            acuteAnglePtrs[acuteSize] = acuteAngle[acuteSize];
+            acuteSize++;
+        } else if (a * a + b * b < c * c) {
+            strcpy(obtuseAngle[obtuseSize], name);
+            obtuseAnglePtrs[obtuseSize] = obtuseAngle[obtuseSize];
+            obtuseSize++;
+        } else if (a * a + b * b == c * c) {
+            strcpy(rightAngle[rightSize], name);
+            rightAnglePtrs[rightSize] = rightAngle[rightSize];
+            rightSize++;
         }
     }
 
-    // Print results
-    if (strlen(notTriangle) > 0) {
-        notTriangle[strlen(notTriangle) - 1] = '\0';
-        printf("Not Triangle: %s\n", notTriangle);
-    } else {
-        printf("Not Triangle: None\n");
-    }
+    // Sort all lists alphabetically
+    if (notTriangleSize > 0) qsort(notTrianglePtrs, notTriangleSize, sizeof(char *), compareNames);
+    if (acuteSize > 0) qsort(acuteAnglePtrs, acuteSize, sizeof(char *), compareNames);
+    if (obtuseSize > 0) qsort(obtuseAnglePtrs, obtuseSize, sizeof(char *), compareNames);
+    if (rightSize > 0) qsort(rightAnglePtrs, rightSize, sizeof(char *), compareNames);
 
-    if (strlen(acuteAngle) > 0) {
-        acuteAngle[strlen(acuteAngle) - 1] = '\0';
-        printf("Acute Angle: %s\n", acuteAngle);
-    } else {
-        printf("Acute Angle: None\n");
-    }
-
-    if (strlen(obtuseAngle) > 0) {
-        obtuseAngle[strlen(obtuseAngle) - 1] = '\0';
-        printf("Obtuse Angle: %s\n", obtuseAngle);
-    } else {
-        printf("Obtuse Angle: None\n");
-    }
-
-    if (strlen(rightAngle) > 0) {
-        rightAngle[strlen(rightAngle) - 1] = '\0';
-        printf("Right Angle: %s\n", rightAngle);
-    } else {
-        printf("Right Angle: None\n");
-    }
+    // Print the lists
+    printList("Not Triangle", notTrianglePtrs, notTriangleSize);
+    printList("Acute Angle", acuteAnglePtrs, acuteSize);
+    printList("Obtuse Angle", obtuseAnglePtrs, obtuseSize);
+    printList("Right Angle", rightAnglePtrs, rightSize);
 
     return 0;
 }

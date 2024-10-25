e8853576495ee2e547402f64e04834acfd08a452
qqaazz800624 <qqaazz800624@gmail.com>
2024-10-25 23:10:59 +0800

    update test

--- hw2/test.c
+++ hw2/test.c
@@ -4,51 +4,39 @@
 
 int main(){
 
-    int N;
-    char C1;
-    double F;
-    char S1[255];
-    char C2;
-    char S2[127];
+    int N, K;
+    int *scores;
     int i;
+    int median;
 
     scanf("%d", &N);
-    scanf(" %c", &C1);
-    scanf("%lf", &F);
-    getchar();  // consume the newline character
-    fgets(S1, sizeof(S1), stdin);  // read a line of text
-    scanf(" %c", &C2);
-    scanf("%s", S2);  // read a single word
+    scanf("%d", &K);
 
-    printf("%.2lf\n", N + F);
+    scores = (int*)malloc(sizeof(int) * N);
+    for (i = 0; i < N; i++){
+        scanf("%d", &scores[i]);
+    }
 
-    int second;
-    second = N + (int)C1;
-    printf("%c %d\n", (char)second, second);
-
-    int third;
-    third = (int)C2 + (int)F;
-    printf("%c %d\n", (char)third, third);
-
-    for (i = 0; i < 255; i++){
-        if (S1[i] >= 'A' && S1[i] <= 'Z'){
-            S1[i] += 32;
+    if (N % 2 == 0){
+        median = scores[N/2 - 1];
+        printf("%d\n", median);
+        if (K > median){
+            printf("WINNER WINNER CHICKEN DINNER!\n");
         }
-        if (S1[i] == '\0'){
-            break;
+        else{
+            printf("BETTER LUCK NEXT TIME!\n");
+        }
+    } else {
+        median = scores[(N-1)/2];
+        printf("%d\n", median);
+        if (K > median){
+            printf("WINNER WINNER CHICKEN DINNER!\n");
+        }
+        else{
+            printf("BETTER LUCK NEXT TIME!\n");
         }
     }
-    printf("%s", S1);
-
-    for (i = 0; i < 127; i++){
-        if (S2[i] >= 'a' && S2[i] <= 'z'){
-            S2[i] -= 32;
-        }
-        if (S2[i] == '\0'){
-            break;
-        }
-    }
-    printf("%s\n", S2);
+    free(scores);
 
     return 0;
 }

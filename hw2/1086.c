bab3e4bbfff28dcc57286d8241af838d75857798
qqaazz800624 <qqaazz800624@gmail.com>
2024-10-25 22:55:51 +0800

    update No. 1086

--- hw2/1086.c
+++ hw2/1086.c
@@ -2,52 +2,53 @@
 #include <stdlib.h>
 
 
-int main() 
-{
- int N;
- char C1;
- double F;
- char S1[255];
- char C2;
- char S2[127];
- //double first;
- int i;
- 
- scanf("%d",&N);
- scanf(" %c",&C1);
- scanf("%lf",&F);
- getchar();
- fgets(S1, sizeof(S1), stdin);
- scanf(" %c",&C2);
- scanf("%s",S2);
- 
- printf("%.2lf\n", N+F);
- 
- int second=N+(int)C1;
- printf("%c %d\n",(char)second,second);
- 
- int third=(int)F+(int)C2;
- printf("%c %d\n",(char)third,third);
- 
- for (i=0;i<255;i++){
-  if(S1[i]>='A' && S1[i]<='Z'){
-   S1[i]+=32;
-  }
-  if(S1[i]=='\0'){
-   break;
-  }
- }
- printf("%s", S1);
- 
- for (i=0;i<127;i++){
-  if(S2[i]>='a' && S2[i]<='z'){
-   S2[i]-=32;
-  }
-  if(S2[i]=='\0'){
-   break;
-  }
- }
- printf("%s\n", S2);
- 
- return 0;
+int main(){
+
+    int N;
+    char C1;
+    double F;
+    char S1[255];
+    char C2;
+    char S2[127];
+    int i;
+
+    scanf("%d", &N);
+    scanf(" %c", &C1);
+    scanf("%lf", &F);
+    getchar();  // consume the newline character
+    fgets(S1, sizeof(S1), stdin);  // read a line of text
+    scanf(" %c", &C2);
+    scanf("%s", S2);  // read a single word
+
+    printf("%.2lf\n", N + F);
+
+    int second;
+    second = N + (int)C1;
+    printf("%c %d\n", (char)second, second);
+
+    int third;
+    third = (int)C2 + (int)F;
+    printf("%c %d\n", (char)third, third);
+
+    for (i = 0; i < 255; i++){
+        if (S1[i] >= 'A' && S1[i] <= 'Z'){
+            S1[i] += 32;
+        }
+        if (S1[i] == '\0'){
+            break;
+        }
+    }
+    printf("%s", S1);
+
+    for (i = 0; i < 127; i++){
+        if (S2[i] >= 'a' && S2[i] <= 'z'){
+            S2[i] -= 32;
+        }
+        if (S2[i] == '\0'){
+            break;
+        }
+    }
+    printf("%s\n", S2);
+
+    return 0;
 }

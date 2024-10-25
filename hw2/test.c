9728d260492a72f09677d94dfc0e4e60ed03557e
qqaazz800624 <qqaazz800624@gmail.com>
2024-10-25 22:56:01 +0800

    update test

--- hw2/test.c
+++ hw2/test.c
@@ -1,48 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-#define MAC_ADDR_LENGTH 18  // 17 characters for MAC address + 1 for null terminator
 
-int main() {
-    int n;
-    
-    // Step 1: Read the number of connected users (n)
-    scanf("%d", &n);
-    
-    // Step 2: Dynamically allocate memory for n MAC addresses
-    char **mac_addresses = (char **)malloc(n * sizeof(char *));
-    if (mac_addresses == NULL) {
-        fprintf(stderr, "Memory allocation failed for mac_addresses.\n");
-        return 1;
-    }
+int main(){
 
-    // Allocate memory for each MAC address (18 bytes for each string)
-    for (int i = 0; i < n; ++i) {
-        mac_addresses[i] = (char *)malloc(MAC_ADDR_LENGTH * sizeof(char));
-        if (mac_addresses[i] == NULL) {
-            fprintf(stderr, "Memory allocation failed for mac_addresses[%d].\n", i);
-            return 1;
+    int N;
+    char C1;
+    double F;
+    char S1[255];
+    char C2;
+    char S2[127];
+    int i;
+
+    scanf("%d", &N);
+    scanf(" %c", &C1);
+    scanf("%lf", &F);
+    getchar();  // consume the newline character
+    fgets(S1, sizeof(S1), stdin);  // read a line of text
+    scanf(" %c", &C2);
+    scanf("%s", S2);  // read a single word
+
+    printf("%.2lf\n", N + F);
+
+    int second;
+    second = N + (int)C1;
+    printf("%c %d\n", (char)second, second);
+
+    int third;
+    third = (int)C2 + (int)F;
+    printf("%c %d\n", (char)third, third);
+
+    for (i = 0; i < 255; i++){
+        if (S1[i] >= 'A' && S1[i] <= 'Z'){
+            S1[i] += 32;
         }
-        // Read the MAC address
-        scanf("%s", mac_addresses[i]);
-    }
-
-    // Step 3: Read the list of query indices (space-separated values)
-    int index;
-    while (scanf("%d", &index) != EOF) {
-        if (index >= 0 && index < n) {
-            printf("%s\n", mac_addresses[index]);  // Print the corresponding MAC address
-        } else {
-            fprintf(stderr, "Invalid index: %d\n", index);
+        if (S1[i] == '\0'){
+            break;
         }
     }
+    printf("%s", S1);
 
-    // Step 4: Free dynamically allocated memory
-    for (int i = 0; i < n; ++i) {
-        free(mac_addresses[i]);  // Free each individual MAC address
+    for (i = 0; i < 127; i++){
+        if (S2[i] >= 'a' && S2[i] <= 'z'){
+            S2[i] -= 32;
+        }
+        if (S2[i] == '\0'){
+            break;
+        }
     }
-    free(mac_addresses);  // Free the array of pointers
+    printf("%s\n", S2);
 
     return 0;
 }

ad0c35ae2939f9d03dd2bab3c72cca833fd2c829
qqaazz800624 <qqaazz800624@gmail.com>
2024-10-27 22:54:11 +0800

    update No. 3042

--- hw2/3042.c
+++ hw2/3042.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 #include <string.h>
+#include <ctype.h>
+
 
 char* read_input_dynamic();
 char* decipher(char *str);
 
-int main(){
-
+int main() {
+    
     char **input_list = NULL;
-    size_t input_count = 0;
-    size_t input_capacity = 20;
-    size_t i;
+    int input_count = 0;
+    size_t INPUT_CAPACITY = 20;
+    int i;
 
-    input_list = (char **)malloc(input_capacity * sizeof(char *));
-    if (!input_list){
+    input_list = (char **)malloc(INPUT_CAPACITY * sizeof(char *));
+    if (!input_list) {
         fprintf(stderr, "Memory allocation failed for input list.\n");
         return 1;
     }
@@ -33,34 +34,30 @@ int main(){
             break;
         }
 
-        if (input_count >= input_capacity){
-            input_capacity *= 2;
-            input_list = (char **)realloc(input_list, input_capacity * sizeof(char *));
+        if (input_count >= INPUT_CAPACITY){
+            INPUT_CAPACITY *= 2;
+            input_list = (char **)realloc(input_list, INPUT_CAPACITY * sizeof(char *));
             if (!input_list){
                 fprintf(stderr, "Memory reallocation failed for input list.\n");
                 return 1;
             }
         }
         input_list[input_count++] = input;
-
     }
 
     for (i=0; i<input_count; ++i){
-        char *deciphered_message = NULL;
-        deciphered_message = decipher(input_list[i]);
-        if (!deciphered_message){
+        char *decipherd_message = NULL;
+        decipherd_message = decipher(input_list[i]);
+        if (!decipherd_message){
             fprintf(stderr, "Error deciphering message.\n");
             return 1;
         }
-
         if (i > 0){
             printf(" ");
         }
-        printf("%s", deciphered_message);
-
-        free(deciphered_message);
+        printf("%s", decipherd_message);
+        free(decipherd_message);
     }
-
     printf("\n");
 
     for (i=0; i<input_count; ++i){
@@ -73,21 +70,18 @@ int main(){
 
 char* read_input_dynamic(){
     size_t buffer_size = 20;
-    size_t position = 0;
     char *buffer = NULL;
+    int ch;
+    int position = 0;
 
     buffer = (char *)malloc(buffer_size * sizeof(char));
-
     if (!buffer) {
         fprintf(stderr, "Memory allocation failed for input buffer.\n");
         return NULL;
     }
 
-    int ch;
-
     while ((ch = getchar()) != EOF && ch != '\n'){
         buffer[position++] = (char)ch;
-
         if (position >= buffer_size){
             buffer_size *= 2;
             buffer = (char *)realloc(buffer, buffer_size * sizeof(char));
@@ -102,18 +96,17 @@ char* read_input_dynamic(){
 }
 
 char* decipher(char *str){
-    size_t length = strlen(str);
+    size_t len = strlen(str);
     char *result = NULL;
-    size_t i;
+    int i;
 
-    result = (char *)malloc((length + 1)*sizeof(char));
-
+    result = (char *)malloc((len+1) * sizeof(char));
     if (!result){
         fprintf(stderr, "Memory allocation failed for decipher buffer.\n");
         return NULL;
     }
 
-    for (i=0; i<length; ++i){
+    for (i=0; i<len; ++i){
         if (isalpha(str[i])){
             char ch = tolower(str[i]);
             if (ch >= 'a' && ch <= 'c'){
@@ -126,6 +119,6 @@ char* decipher(char *str){
             result[i] = str[i];
         }
     }
-    result[length] = '\0';
+    result[len] = '\0';
     return result;
 }

f9f725f231d0c674952987b92fd0441f72f06724
qqaazz800624 <qqaazz800624@gmail.com>
2024-10-29 17:37:58 +0800

    add No. 1094

---
+++ ch9/1094.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int n;
+    cin >> n;
+    string temp;
+    string *p;
+    
+    p = new string[n];
+    for (int i = 0; i < n; i++) {
+        cin >> p[i];
+    }
+
+    //bubble sort 
+    bool swapped;
+    for (int j=n; j>1; j--){
+        swapped = false;
+        for (int i=0; i<j-1; i++){
+            if (p[i] > p[i+1]){
+                temp = p[i];
+                p[i] = p[i+1];
+                p[i+1] = temp;
+                swapped = true;
+            }
+        }
+        if (!swapped){
+            break;
+        }
+    }
+    
+
+    for (int i=0; i<n; i++){
+        cout << p[i] << " ";
+    }
+    cout << endl;
+
+    delete[] p;
+
+    return 0;
+}

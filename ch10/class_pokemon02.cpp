db09e92f4f958739ae787633355e17ba814f1392
qqaazz800624 <qqaazz800624@gmail.com>
2024-10-31 12:05:52 +0800

    finish pokemon02 exercise

--- ch10/class_pokemon02.cpp
+++ ch10/class_pokemon02.cpp
@@ -17,9 +17,32 @@ class Pokemon {
             HpCur = hpc;
             HpMax = hpm;
         };
-        void Attack(Pokemon &target);
-        void Defense(int n);
-        void Cure();
+
+        void Attack(Pokemon &target){
+            if (HpCur <= 0){
+                cout << Name << " cannot attack." << endl;
+                return;
+            }
+            if (target.HpCur <= 0){
+                cout << "Target: " << target.Name << " is fainted, cannot attack." << endl;
+                return;
+            }
+            cout << Name << " attacks " << target.Name << " " << Lv << " points." << endl;
+            target.Defense(Lv);
+        };
+
+        void Defense(int n){
+            HpCur -= n;
+            if (HpCur <= 0){
+                cout << Name << " fainted." << endl;
+                HpCur = 0;
+            }
+        };
+
+        void Cure(){
+            cout << Name << " is cured." << endl;
+            HpCur = HpMax;
+        };
 
     private:
         string Name;
@@ -38,8 +61,16 @@ void Pokemon::Show(){
 int main(){
 
     Pokemon p1, p2;
-    p1.SetData("Pikachu", 10, 15, 14);
+    p1.SetData("Pikachu", 10, 15, 15);
     p2.SetData("Charmander", 15, 25, 25);
+
+    p1.Attack(p2);
+    p2.Attack(p1);
+    p1.Attack(p2);
+    p2.Attack(p1);
+
+    p1.Cure();
+
     p1.Show();
     p2.Show();
     

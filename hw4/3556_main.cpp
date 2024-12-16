f819189fdf368efdea8dcdff7fd7c6743922273f
qqaazz800624 <qqaazz800624@gmail.com>
2024-12-16 11:47:38 +0800

    update No. 3556

--- hw4/3556_main.cpp
+++ hw4/3556_main.cpp
@@ -1,4 +1,3 @@
-//輸入今日出席人數&候選法案數
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
@@ -15,21 +14,18 @@ int main(){
     cin.ignore();
     int members = atoi(member.c_str());
     int candidates = atoi(candidate.c_str());
- 
+
     Vote_sys vs(members, candidates);
     while(getline(cin, str_in)){
         if(str_in == "q"){
             break;
         }
-        // cout << str_in << endl;    
         string ID = str_in.substr(0, str_in.find(' '));
         string choice = str_in.substr(str_in.find(' ')+1, str_in.length());
-        // cout << ID << " " << choice << endl;
  
         Ballot b(ID, choice);
         vs.add_ballot(b);
     }
     vs.display();
     vs.show_result();
- 
 }
